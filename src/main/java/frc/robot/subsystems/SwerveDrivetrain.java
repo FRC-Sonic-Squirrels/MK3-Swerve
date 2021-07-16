4d65a27f383ee9cbab6ad6d93645a4d07c718a94
Bryn Dole <bryn.dole@gmail.com>
2021-07-16 11:34:39 -0700

    set CAN ids

--- src/main/java/frc/robot/subsystems/SwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/SwerveDrivetrain.java
@@ -33,24 +33,23 @@ public class SwerveDrivetrain extends SubsystemBase {
   public static double backLeftOffset = 0;
   public static double backRightOffset = 0;
 
+  //put your can Id's here!
+  public static final int frontLeftDriveId = 1;
+  public static final int frontLeftCANCoderId = 21;
+  public static final int frontLeftSteerId = 11;
+  //put your can Id's here!
+  public static final int frontRightDriveId = 2;
+  public static final int frontRightCANCoderId = 22;
+  public static final int frontRightSteerId = 12;
+  //put your can Id's here!
+  public static final int backLeftDriveId = 4;
+  public static final int backLeftCANCoderId = 24;
+  public static final int backLeftSteerId = 14;
+  //put your can Id's here!
+  public static final int backRightDriveId = 3;
+  public static final int backRightCANCoderId = 23;
+  public static final int backRightSteerId = 13;
 
-  //put your can Id's here!
-  public static final int frontLeftDriveId = 1; 
-  public static final int frontLeftCANCoderId = 2; 
-  public static final int frontLeftSteerId = 3;
-  //put your can Id's here!
-  public static final int frontRightDriveId = 4; 
-  public static final int frontRightCANCoderId = 5; 
-  public static final int frontRightSteerId = 6; 
-  //put your can Id's here!
-  public static final int backLeftDriveId = 10; 
-  public static final int backLeftCANCoderId = 11; 
-  public static final int backLeftSteerId = 12;
-  //put your can Id's here!
-
-  public static final int backRightDriveId = 7; 
-  public static final int backRightCANCoderId = 8; 
-  public static final int backRightSteerId = 9;   
   public static AHRS gyro = new AHRS(SPI.Port.kMXP);
 
   private SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
