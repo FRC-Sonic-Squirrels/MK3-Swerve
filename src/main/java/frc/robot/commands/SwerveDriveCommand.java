71dd99c5662e5d2eddba8cbb618e5244f651bf7a
Bryn Dole <bryn.dole@gmail.com>
2021-11-19 12:50:00 -0800

    tune nerfing of robot speed

--- src/main/java/frc/robot/commands/SwerveDriveCommand.java
+++ src/main/java/frc/robot/commands/SwerveDriveCommand.java
@@ -12,17 +12,27 @@ public class SwerveDriveCommand extends CommandBase {
 
   private final SwerveDrivetrain drivetrain;
   private final XboxController controller;
+  private double nerfStrafe = 0.25;
+  private double nerfRotation = 0.5;
 
-  // Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0 to 1.
-  private final SlewRateLimiter xspeedLimiter = new SlewRateLimiter(6);
-  private final SlewRateLimiter yspeedLimiter = new SlewRateLimiter(6);
-  private final SlewRateLimiter rotLimiter = new SlewRateLimiter(6);
+  // Slew rate limiters to make joystick inputs more gentle; 
+  // setting to 6 gives 1/3 sec from 0 to 1.
+  private final SlewRateLimiter xspeedLimiter = new SlewRateLimiter(3);
+  private final SlewRateLimiter yspeedLimiter = new SlewRateLimiter(3);
+  private final SlewRateLimiter rotLimiter = new SlewRateLimiter(3);
 
   public SwerveDriveCommand(SwerveDrivetrain drivetrain, XboxController controller) {
     this.drivetrain = drivetrain;
     addRequirements(drivetrain);
-
     this.controller = controller;
+
+    SmartDashboard.putNumber("nerf_stafe", nerfStrafe);
+    SmartDashboard.putNumber("nerf_rotation", nerfRotation);
+    SmartDashboard.putNumber("JoyX", 0.0);
+    SmartDashboard.putNumber("JoyY", 0.0);
+    SmartDashboard.putNumber("xSpeed", 0.0);
+    SmartDashboard.putNumber("ySpeed", 0.0);
+    SmartDashboard.putNumber("rotation", 0.0);
   }
 
   @Override
@@ -30,7 +40,7 @@ public class SwerveDriveCommand extends CommandBase {
     // Get the x speed. We are inverting this because Xbox controllers return
     // negative values when we push forward.
     double joyY = controller.getY(GenericHID.Hand.kLeft);
-    if (Math.abs(joyY) < 0.02) {
+    if (Math.abs(joyY) < 0.1) {
       joyY = 0.0;
     }
     double xSpeed = 
@@ -41,7 +51,7 @@ public class SwerveDriveCommand extends CommandBase {
     // we want a positive value when we pull to the left. Xbox controllers
     // return positive values when you pull to the right by default.
     double joyX = controller.getX(GenericHID.Hand.kLeft);
-    if (Math.abs(joyX) < 0.02) {
+    if (Math.abs(joyX) < 0.1) {
       joyX = 0.0;
     }
     double ySpeed =
@@ -58,15 +68,17 @@ public class SwerveDriveCommand extends CommandBase {
 
     boolean calibrate = controller.getBumper(GenericHID.Hand.kLeft);
 
-    // TODO: undo nerfing speed
-    double nerfStrafe = 0.1;
-    double nerfRotation = 0.25;
     xSpeed = nerfStrafe * xSpeed;
     ySpeed = nerfStrafe * ySpeed;
     rot = nerfRotation * rot;
 
     drivetrain.drive(xSpeed, ySpeed, rot, true, calibrate);
-    
+
+    SmartDashboard.putNumber("JoyX", joyX);
+    SmartDashboard.putNumber("JoyY", joyY);
+    SmartDashboard.putNumber("xSpeed", xSpeed);
+    SmartDashboard.putNumber("ySpeed", ySpeed);
+    SmartDashboard.putNumber("rotation", rot);
   }
 
 }
